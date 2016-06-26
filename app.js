61cefa5565650ec70d53664b22668935b7c5e210
Girma Nigusse <xgirma@gmail.com>
2016-06-26 10:48:28 -0400

    restructured folder

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ var logger        = require('morgan');
 var cookieParser  = require('cookie-parser');
 var bodyParser    = require('body-parser');
 
-var routes        = require('./routes/index');
-var users         = require('./routes/users');
+var routes        = require('./server/routes/index');
+var users         = require('./server/routes/users');
 
 var app           = express();
 
@@ -17,7 +17,7 @@ app.set('views', path.join(__dirname, 'server/views'));
 app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 3000);
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'client', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
