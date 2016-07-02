02ed550d5f0f4278558e39dbd98947f5d5bef597
Girma Nigusse <xgirma@gmail.com>
2016-07-02 13:13:20 -0400

    cleanup

--- app.js
+++ app.js
@@ -18,11 +18,11 @@ app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 3000);
 
 app.use(favicon(path.join(__dirname, 'client', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
